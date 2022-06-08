137b92b04902ae9f83ee5cde4a729e59cf4bd1bc
ianplus <86130139+ianplus@users.noreply.github.com>
2022-06-07 23:42:53 -0300

    Update rechanger.js

--- rechanger.js
+++ rechanger.js
@@ -8,6 +8,7 @@ function extraerParametros(url) {
 }
 
 try {
+  document.getElementById("demo").innerHTML = newURL.toString();
   console.log(newURL);
   console.log(extraerParametros(newURL));
 } catch (e) {
