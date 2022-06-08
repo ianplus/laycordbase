4d464792ae0227bab69ff843c4edc77fb68c5bac
ianplus <86130139+ianplus@users.noreply.github.com>
2022-06-07 23:31:49 -0300

    Update rechanger.js

--- rechanger.js
+++ rechanger.js
@@ -1,1 +1,15 @@
-document.getElementById('1').innerText += " link";
+var newURL = window.location.href + "?p1=a&p2=b"
+
+function extraerParametros(url) {
+  if (typeof url != 'string') {
+    throw TypeError('precisa ser una cadena');
+  }
+  return (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce((a, p) => ((a[p.slice(0, p.indexOf("="))] = p.slice(p.indexOf('=') + 1)), a), {});
+}
+
+try {
+  console.log(newURL);
+  console.log(extraerParametros(newURL));
+} catch (e) {
+  console.log(`Error: ${e.message}`);
+}
